8e05a01607ee46a399de8d1c447acd45c375ebd9
lilray <2313715475@qq.com>
2023-11-08 15:36:41 +0800

    整合测试Redis

--- usercenter/src/test/java/com/ray/usercenter/service/RedisTest.java
+++ usercenter/src/test/java/com/ray/usercenter/service/RedisTest.java
@@ -1,8 +1,11 @@
 package com.ray.usercenter.service;
 
+import com.ray.usercenter.model.domain.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 import javax.annotation.Resource;
 
@@ -20,8 +23,30 @@ public class RedisTest {
     @Resource
     private RedisTemplate redisTemplate;
 
-    @Test
-    public void test(){
 
+    @Test
+    void test() {
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        // 增
+        valueOperations.set("shayuString", "fish");
+        valueOperations.set("shayuInt", 1);
+        valueOperations.set("shayuDouble", 2.0);
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("shayu");
+        valueOperations.set("shayuUser", user);
+
+        // 查
+        Object shayu = valueOperations.get("shayuString");
+        Assertions.assertTrue("fish".equals((String) shayu));
+        shayu = valueOperations.get("shayuInt");
+        Assertions.assertTrue(1 == (Integer) shayu);
+        shayu = valueOperations.get("shayuDouble");
+        Assertions.assertTrue(2.0 == (Double) shayu);
+        System.out.println(valueOperations.get("shayuUser"));
+        valueOperations.set("shayuString", "fish");
+
+        //删
+//        redisTemplate.delete("shayuString");
     }
 }
